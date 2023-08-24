8d254cd0b36ed551b856b2faf831f197ce7dcb4f
salahbm <salahbm.001@gmail.com>
2023-08-24 11:04:05 +0500

    lib and fidomodule changed

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
   return (
     <SafeAreaView>
       <FidoModuleButton />
-      <RegisterCredentialScreen />
+      {/* <RegisterCredentialScreen /> */}
     </SafeAreaView>
   );
 }
--- components/FidoModule.tsx
+++ components/FidoModule.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import {NativeModules, Button, Alert, SafeAreaView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  NativeModules,
+  View,
+  TextInput,
+  Text,
+  Button,
+  Alert,
+  SafeAreaView,
+} from 'react-native';
+import axios from 'axios';
 
-const {TrustKeyApiBridge, FidoTrustkeyBridge} = NativeModules;
+const {TrustKeyApiBridge} = NativeModules;
 
 const FidoModuleButton = () => {
+  const [registerResult, setRegisterResult] = useState('');
+  const [name, setName] = useState('');
   const InitilizeFidoDevice = async () => {
     try {
       await TrustKeyApiBridge.initFidoDevice('testName', 'testLocation');
@@ -16,10 +27,6 @@ const FidoModuleButton = () => {
       );
     }
   };
-  const DeviceHandle = () => {
-    FidoTrustkeyBridge.preMakeCredential('user123');
-    FidoTrustkeyBridge.preGetAssertion('user456');
-  };
   const Register = async () => {
     try {
       const credential = await TrustKeyApiBridge.makeCredentialCTAPLog();
@@ -29,12 +36,129 @@ const FidoModuleButton = () => {
       console.error(error);
     }
   };
-  const onDisconnect = async () => {
+
+  const generateUniqueString = (length: number) => {
+    const characters =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  };
+
+  const registerCredential = async () => {
+    const challenge = generateUniqueString(64);
+    const user = {
+      id: generateUniqueString(16),
+      name: 'user@example.com',
+      displayName: name,
+    };
+    const rp = {
+      name: 'Example RP',
+    };
+    const pubKeyCredParams = [
+      {type: 'public-key', alg: -7},
+      {type: 'public-key', alg: -35},
+      {type: 'public-key', alg: -36},
+      {type: 'public-key', alg: -257},
+      {type: 'public-key', alg: -258},
+      {type: 'public-key', alg: -259},
+      {type: 'public-key', alg: -37},
+      {type: 'public-key', alg: -38},
+      {type: 'public-key', alg: -39},
+      {type: 'public-key', alg: -65535},
+      {type: 'public-key', alg: -261},
+      {type: 'public-key', alg: -260},
+    ];
+    const options = {
+      challenge,
+      user,
+      rp,
+      pubKeyCredParams,
+      attestation: 'none',
+      authenticatorSelection: {
+        authenticatorAttachment: 'cross-platform',
+        userVerification: 'preferred',
+      },
+      timeout: 60000,
+      excludeCredentials: [],
+    };
+
     try {
-      const exit = await TrustKeyApiBridge.disconnect();
-      Alert.alert('success', exit);
+      await TrustKeyApiBridge.preMakeCredentialProcess();
+      const result = await TrustKeyApiBridge.postMakeCredentialProcess(options);
+      setRegisterResult(result);
+      Alert.alert('Success', 'Credential registered successfully!');
     } catch (error) {
-      Alert.alert('error');
+      console.error('Credential registration error:', error);
+      Alert.alert('Error', 'An error occurred during credential registration.');
+    }
+  };
+
+  const preMakeCredentialProcess = async () => {
+    try {
+      await TrustKeyApiBridge.TKAuthN_GetDeviceHandle();
+
+      const urlPath =
+        'https://demo.trustkeysolutions.com:12001/FidoDemo/demoDeveloper/fidoRegRequest.jsp';
+      const formData = new URLSearchParams();
+      formData.append('name', generateUniqueString(8));
+      formData.append('displayName', name);
+      formData.append('options', 'true');
+
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        Accept: 'text/*',
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.0;* Windows NT)',
+      };
+
+      const response = await axios.post(urlPath, formData.toString(), {
+        headers,
+      });
+
+      const success = await TrustKeyApiBridge.TKAuthN_Fido_MakeCredential(
+        response.data,
+      );
+      if (success) {
+        postMakeCredentialProcess(
+          TrustKeyApiBridge.m_makeCredential_CTAP_Result,
+        );
+        Alert.alert('success');
+      } else {
+        Alert.alert('failed');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const postMakeCredentialProcess = async (pszCredential: Uint8Array) => {
+    try {
+      const urlPath =
+        'https://demo.trustkeysolutions.com:12001/FidoDemo/demoDeveloper/fidoRegProc.jsp';
+      const formData = new URLSearchParams();
+      formData.append('publicKeyCredential', pszCredential.toString());
+
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        Accept: 'text/*',
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.0;* Windows NT)',
+      };
+
+      const response = await axios.post(urlPath, formData.toString(), {
+        headers,
+      });
+
+      if (response.data.includes('1200')) {
+        Alert.alert('success');
+      } else {
+        Alert.alert('failed');
+      }
+    } catch (error) {
       console.error(error);
     }
   };
@@ -46,14 +170,27 @@ const FidoModuleButton = () => {
         color="green"
         onPress={InitilizeFidoDevice}
       />
-      {/* <Button
-        title="Device Disconnect Testing"
-        color="red"
-        onPress={onDisconnect}
-      /> */}
 
-      {/* <Button title="Device Handle" color="blue" onPress={DeviceHandle} /> */}
       <Button title="Register" color="brown" onPress={Register} />
+      <View style={{padding: 10, gap: 10}}>
+        <TextInput
+          style={{
+            fontSize: 12,
+            color: 'black',
+            borderWidth: 1,
+            borderRadius: 5,
+          }}
+          value={name}
+          onChangeText={val => setName(val)}
+          placeholder="Enter Display name"
+        />
+        <Button
+          title="Register Credential"
+          onPress={preMakeCredentialProcess}
+          disabled={!name}
+        />
+        <Text dataDetectorType={'email'}>{registerResult}</Text>
+      </View>
     </SafeAreaView>
   );
 };
